5d7aac3259593d2dd7dce0a17b0ffaf2aff7641b
ChristinaMartin13 <christina13martin@gmail>
2023-11-26 17:35:45 -0600

    Completed Task 1

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -3,6 +3,8 @@
 
 
 --Part 1: List the columns and their data types in the Jobs table.
+--Column 1: Id          Column 2: Name       Column 3: EmployerId
+--DataType: int AI PK   DataType: longtext   Datatype: int
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
 
