117b64256ea2d44c29911ac40c384707d48dacf1
ChristinaMartin13 <christina13martin@gmail>
2023-11-26 19:25:01 -0600

    Completed half of Task 2

--- TechJobs6Persistent/Controllers/EmployerController.cs
+++ TechJobs6Persistent/Controllers/EmployerController.cs
@@ -13,31 +13,63 @@ using TechJobs6Persistent.ViewModels;
 namespace TechJobs6Persistent.Controllers
 {
     public class EmployerController : Controller
-    { 
+    {
+        private readonly JobDbContext _dbContext;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var employers = _dbContext.Employers.ToList();
+            return View(employers);
         }
 
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            var model = new AddEmployerViewModel();
+            return View(model);
         }
 
         [HttpPost]
-        public IActionResult ProcessCreateEmployerForm()
+        public IActionResult ProcessCreateEmployerForm(AddEmployerViewModel model)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var newEmployer = new Employer
+                {
+                    Name = model.Name,
+                    Location = model.Location
+                };
+
+                _dbContext.Employers.Add(newEmployer);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View("Create", model);
         }
+
 
         public IActionResult About(int id)
         {
-            return View();
-        }
+            var employer = _dbContext.Employers.Find(id);
 
+            if (employer == null)
+            {
+                return NotFound();
+            }
+
+            return View(employer);
+        }
     }
+
 }
+
 
---
+++ TechJobs6Persistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TechJobs6Persistent.Models;
+
+namespace TechJobs6Persistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage ="Employer name is required.")]
+        [StringLength(50,ErrorMessage = "Employer name must not exceed 50 characters.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Employer location is required.")]
+        [StringLength(50, ErrorMessage = "Employer location must not exceed 50 characters.")]
+        public string? Location { get; set; }
+    }
+
+    
+}
